3e2b0f9a36e6ef0482988035acf4c2713ac80288
Dariaaal <1406dariaal@gmail.com>
2023-03-27 00:41:25 +0300

    search

---
+++ src/components/api/SearchQueryMovies.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const BASE_URL = 'https://api.themoviedb.org/';
+
+const API_KEY = '593700439a076bb032ad3ac2a00bd9c1';
+
+async function getSearchQueryMovies (query) {
+    const response = await axios.get(`${BASE_URL}3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`)
+    return response.data; 
+}
+
+export default getSearchQueryMovies;
+
+// https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
--- src/components/pages/Movies.jsx
+++ src/components/pages/Movies.jsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from "react";
+import css from './Movies.module.css'
+import getSearchQueryMovies from "components/api/SearchQueryMovies";
+
 const Movies = () => {
 
+    const [value, setValue] = useState('');
+    const [movies, setMovies] = useState([]);
+    const [query, setQuery] = useState('');
+
+    useEffect(()=>{
+        async function GetQuery(){
+            const data = await getSearchQueryMovies(value);
+            setMovies([...data.results]);
+            console.log(movies)
+        }
+
+        GetQuery();
+    }, [])
+
+    const handleChange = e => {
+        setValue(e.target.value.toLowerCase());
+      }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setMovies(value);
+        reset();
+    }
+      
+    const reset = () => {
+        setValue('');
+    }
+
+    return <form onSubmit={handleSubmit}>
+
+    <input
+      type="text"
+      autoComplete="off"
+      autoFocus
+      className={css.search}
+      onChange={handleChange}
+      value={value}
+    />
+
+<button type="submit">
+     Search
+    </button>
+
+  </form>
 }
 
 
