5dda8452db7fbb624340440b9db219a6749607f0
Dariaaal <1406dariaal@gmail.com>
2023-03-26 15:22:14 +0300

    макет

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,10 @@
+import {NavLink, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import MovieDetails from './pages/MovieDetails';
+import Cast from './Cast';
+import Reviews from './Reviews';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +17,18 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <nav>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/movies'>Movies</NavLink>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/movies' element={<Movies/>}/>
+        <Route path='/movies/:movieId' element={<MovieDetails/>}>
+          <Route path='/movies/:movieId/cast' element={<div><Cast/></div>}/>
+          <Route path='/movies/:movieId/reviews' element={<div><Reviews/></div>}/>
+        </Route>
+      </Routes>
     </div>
   );
 };
